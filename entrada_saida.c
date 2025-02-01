30acb3a220e986c3ffc04c2d0074f9b229f860ad
Darlan Monteiro <darlanmonteirott@gmail.com>
2025-02-01 00:26:00 -0300

    treino

---
+++ entrada_saida.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main()
+{
+    setlocale(LC_ALL, "Portuguese");
+    printf("FeMASS 2020\n");
+    printf("Programação de Computadores I\n");
+    printf("Professor: Sergio Hermenegildo\n");
+    printf("\n\n\n");
+    printf("10 e do tipo int\n");
+    printf("A \t\t 1,20\n");
+    printf("B \t\t 3,00\n");
+    printf("C \t\t 5,70\n");
+    printf("FIM DA LISTAGEM\n");
+
+    return 0;
+}
