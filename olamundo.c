39ba211abbbbdced4be4598b17219c24935c6834
Darlan Monteiro <darlanmonteirott@gmail.com>
2025-02-01 00:00:56 -0300

    AA

---
+++ olamundo.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <locale.h>
+
+main()
+{
+setlocale(LC_ALL, "Portuguese"); // Configura a localidade para o padr?o do sistema
+printf("Ol�, Mundo.\n");
+    printf("Estamos aqui\n");
+    system("pause");
+
+}
